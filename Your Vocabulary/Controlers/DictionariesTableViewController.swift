faa5bd834a3d639eba29a1fdaac72c471c2e7f32
Dmitrii Semykin <semyda94@yandex.ru>
2018-01-05 01:23:07 +1300

    Added functions add new word (simple word with template "New words" no translation, no definition) and removing words from table

--- Your Vocabulary/Controlers/DictionariesTableViewController.swift
+++ Your Vocabulary/Controlers/DictionariesTableViewController.swift
@@ -78,7 +78,6 @@ class DictionariesTableViewController: UITableViewController {
             // Delete the row from the data source
             dictionaries.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-            tableView.reloadData()
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    

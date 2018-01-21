b6bee91834e03092a497de07e119e8fe54fc092d
Dmitrii Semykin <semyda94@yandex.ru>
2018-01-21 14:08:33 +1300

    Removed a word interface. Add static settings(No any functionality)

--- Your Vocabulary/Controlers/DictionaryPropertiesViewController.swift
+++ Your Vocabulary/Controlers/DictionaryPropertiesViewController.swift
@@ -78,12 +78,6 @@ class DictionaryPropertiesViewController: UIViewController, UITextFieldDelegate 
         // Do any additional setup after loading the view.
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
